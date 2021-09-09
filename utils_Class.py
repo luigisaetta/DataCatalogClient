d2f7427d317389740c6384eeb46e3f367a2d9722
Luigi Saetta <lsaetta@MBP-di-Luigi.local>
2021-09-09 15:05:47 +0200

    modified for Sogei workshop

--- utils_Class.py
+++ utils_Class.py
@@ -3,11 +3,9 @@ import hashlib
 from ads.dataset.factory import DatasetFactory
 import os
 
+
 # utils to read from Data Catalog
 
-# for print formatting
-BOLD = '\033[1m'
-END = '\033[0m'
 
 class  ODSDataCatalog:
     def __init__(self, catalog_id, namespace_id, asset_key):
@@ -22,17 +20,90 @@ class  ODSDataCatalog:
         # assett key, in this case the assett is the bucket
         self.asset_key = asset_key 
     
+    #
+    # print the list of Data Entities names (only the names)for the Data Assett
+    # for more info see next function
+    def print_entity_name_list(self):
+        rps = oci.auth.signers.get_resource_principals_signer()
+    
+        dcat_client = oci.data_catalog.DataCatalogClient({}, signer=rps)
+    
+        list_entities = dcat_client.list_entities(catalog_id=self.catalog_id, data_asset_key=self.asset_key).data
+        
+        entity_name_list = []
+        
+        for elem in list_entities.items:
+            entity_name_list.append(elem.display_name)
+    
+        entity_name_list = sorted(entity_name_list)
+        
+        print("The list of entities is:")
+        print()
+        
+        for name in entity_name_list:
+            print(name)
+            
+        return
+    #
+    # show all metadata associated with the Data Entity (file in a bucket)
+    #
+    def print_entity_list_raw(self):
+        rps = oci.auth.signers.get_resource_principals_signer()
+    
+        dcat_client = oci.data_catalog.DataCatalogClient({}, signer=rps)
+    
+        list_entities = dcat_client.list_entities(catalog_id=self.catalog_id, data_asset_key=self.asset_key).data
+        
+        for elem in list_entities.items:
+            print(elem.display_name)
+            print("-------")
+            print(elem)
+        
     def get_key_from_name(self, name):
         rps = oci.auth.signers.get_resource_principals_signer()
     
         dcat_client = oci.data_catalog.DataCatalogClient({}, signer=rps)
     
         search = dcat_client.list_entities(catalog_id=self.catalog_id, data_asset_key=self.asset_key).data
+        
         key_name = ''
         for elem in search.items:
             if str(elem.display_name) == name:
                 key_name = elem.key # retrieve entity(file) access key
+                
+                # exit from loop
+                break
+        
         return key_name
+    
+    #
+    # To get the custom property owner
+    #
+    def get_owner(self, name):
+        # get the signer
+        rps = oci.auth.signers.get_resource_principals_signer()
+    
+        dcat_client = oci.data_catalog.DataCatalogClient({}, signer=rps)
+    
+        response = dcat_client.get_entity(catalog_id=self.catalog_id, 
+                                  data_asset_key=self.asset_key, 
+                                  entity_key=self.get_key_from_name(name))
+        # md5 read from Catalog
+        search = response.data.custom_property_members
+        
+        for elem in search:
+            
+            if elem.display_name == 'owner':
+                if elem.value == None:
+                    print('No value registered in the data catalog!')
+                    return '-'
+                else:
+                    owner = elem.value
+                
+                # exit from loop
+                break
+                
+        return owner
     
     def get_hash_from_catalog(self, name):
         # get the signer
@@ -45,14 +116,19 @@ class  ODSDataCatalog:
                                   entity_key=self.get_key_from_name(name))
         # md5 read from Catalog
         search = response.data.custom_property_members
+        
         for elem in search:
+            
             if elem.display_name == 'file_hash':
                 if elem.value == None:
-                    print(BOLD+'No value registered in the data catalog!'+END)
-                    return None
+                    print('No value registered in the data catalog!')
+                    return '-'
                 else:
                     md5_cat = elem.value
-    
+                
+                # exit from loop
+                break
+                
         return md5_cat
     
     def get_version_from_catalog(self, name):
@@ -70,7 +146,7 @@ class  ODSDataCatalog:
             if elem.display_name == 'file_version':
                 if elem.value == None:
                     print(BOLD+'No value registered in the data catalog!'+END)
-                    return None
+                    return '-'
                 else:
                     md5_cat = elem.value
     
@@ -100,4 +176,3 @@ class  ODSDataCatalog:
         os.remove(TMP_FILE)
         return md5_computed
                    
-    
